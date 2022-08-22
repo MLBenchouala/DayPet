d5d80d84b6cfb568d7a7f90cf614ef33ff72da8c
mohamed larbi benchouala <mohamedlarbi.benchouala@gmail.com>
2022-08-22 17:02:54 +0200

    added features to models

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :pets
+  resources :pets, only: [:create, :new, :index, :show, :destroy]
+  resources :bookings, only: [:new, :create, :index, :show]
+  resources :pets do
+    resources :ratings, only: [:new, :create, :index, :show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
