e15409b43717c6c8e2b4824d3e28d059f8e9e2d0
arthurcoutas <arthur@coutas.com>
2022-08-23 14:31:14 +0200

    controllers normally finish and dashboard empty

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,27 +1,29 @@
 class BookingsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  before_action :set_booking, only: [:destroy]
 
   def new
-  end
-
-  def update
-  end
-
-  def destroy
+    @booking = Booking.new
+    authorize @booking
   end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    authorize @booking
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render
+    end
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:pet_id, :user_id)
+  end
+
+  def set_booking
+    @booking = booking.find(params[:id])
   end
 end
