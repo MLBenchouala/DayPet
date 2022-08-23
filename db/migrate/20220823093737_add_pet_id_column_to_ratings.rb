1a43c2ea17ff00a948d4e8200edaab695aca5a1e
mohamed larbi benchouala <mohamedlarbi.benchouala@gmail.com>
2022-08-23 11:40:54 +0200

    database adjustment

---
+++ db/migrate/20220823093737_add_pet_id_column_to_ratings.rb
@@ -0,0 +1,5 @@
+class AddPetIdColumnToRatings < ActiveRecord::Migration[7.0]
+  def change
+    add_column :ratings, :Pet_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_142644) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_23_093854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,9 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_142644) do
   create_table "ratings", force: :cascade do |t|
     t.string "comment"
     t.integer "rate"
-    t.integer "booking_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "Pet_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,7 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_142644) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "number"
-    t.float "rating"
     t.string "photo_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
