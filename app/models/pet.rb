b94adc4806f5dfe7bd90268d6aff5cec03d16e3f
mohamed larbi benchouala <mohamedlarbi.benchouala@gmail.com>
2022-08-24 10:43:52 +0200

    tst

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  has_one :booking
+  has_many :bookings
   has_many :ratings, dependent: :destroy
   validates :name, :location, :age, :race, :sexe, :photo_url, :price, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :bookings, only: [:new, :create, :index, :show]
   resources :pets
-  resources :ratings
+  resources :bookings do
+    resources :ratings, only: [:new, :create]
+  end
+  resources :pets do
+    resources :ratings, only: [:index, :show]
+  end
 end
